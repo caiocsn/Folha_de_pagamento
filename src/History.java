324be5022b46ff87bf8082e8f317fdf6d7558ce6
Caio Nunes <caiocsn@hotmail.com>
2017-07-28 11:01:10 -0300

    first commit

---
+++ src/History.java
@@ -0,0 +1,16 @@
+
+public class History {
+	// This class implements a log of monthly payments
+	float hours;
+	float comission;
+	float sales;
+	float synd_tax;
+
+	public History(){
+		this.hours = 0.0;
+		this.comission = 0.0;
+		this.sales = 0.0;
+		this.synd_tax = 0.0;
+	}
+
+}
